f33222ac141e880276e0aeb2219cbe61893cbcd1
TatianaGantievskaya <ta.gantievskaya@gmail.com>
2018-03-02 17:22:39 +0300

    add the task

---
+++ TheLongestSubsequence/EntryPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TheLongestSubsequence
+{
+  /// <summary>
+  /// The entry point of the program.
+  /// </summary>
+  class EntryPoint
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        Sequence inputString = new Sequence(args[0]);
+        inputString.GetMaxAmountOfTheSameConsecutiveSymbols();
+      }
+      catch(Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+    }
+  }
+}
---
+++ TheLongestSubsequence/Sequence.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+
+namespace TheLongestSubsequence
+{
+  /// <summary>
+  /// This class was made for work with the input sequence.
+  /// </summary>
+  class Sequence
+  {
+    /// <summary>
+    /// The input string we work with.
+    /// </summary>
+    public string EnteredSequence { get; set; }
+
+    /// <summary>
+    /// Class constructor with a parameter, which initialize class's field.
+    /// </summary>
+    /// <param name="sequence"> A sequence, which was entered in the command line. </param>
+    public Sequence(string sequence)
+    {
+      EnteredSequence = sequence;
+    }
+
+    protected int FindTheLongestSubsequence()
+    {
+      int amountOfTheSameSymbols = 1;
+      int counter = 1;
+      for (int i = 0; i < EnteredSequence.Length - 1; i++)
+      {
+        if (EnteredSequence[i].Equals(EnteredSequence[i + 1]))
+        {
+          counter++;
+        }
+        if (!EnteredSequence[i].Equals(EnteredSequence[i + 1]) || i.Equals(EnteredSequence.Length - 2))
+        {
+          if (amountOfTheSameSymbols < counter)
+          {
+            amountOfTheSameSymbols = counter;
+          }
+          counter = 1;
+        }
+      }
+      return amountOfTheSameSymbols;
+    }
+
+    protected string BuildTheOutputString()
+    {
+      int theLongestSubsequence = FindTheLongestSubsequence();
+      StringBuilder outputString = new StringBuilder();
+      outputString.Append("The maximum number of identical consecutive characters in the string: ").Append(theLongestSubsequence);
+      return outputString.ToString();
+    }
+
+    /// <summary>
+    /// The method, which outputs the information about the maximum length of a subsequence 
+    /// of the same consecutive symbols containing in the input string.
+    /// </summary>
+    public void GetMaxAmountOfTheSameConsecutiveSymbols()
+    {
+      string outputString = BuildTheOutputString();
+      Console.WriteLine(outputString);
+    }
+  }
+}
