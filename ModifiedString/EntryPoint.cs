1bdfa8bac0a27335d245fa1a12ec9e27c0f4ab6d
TatianaGantievskaya <ta.gantievskaya@gmail.com>
2018-03-04 13:59:28 +0300

    add the task

---
+++ ModifiedString/EntryPoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ModifiedString
+{
+  /// <summary>
+  /// This is entry point of the program.
+  /// </summary>
+  class EntryPoint
+  {
+    static void Main(string[] args)
+    {
+      string inputString = null;
+      while (string.IsNullOrEmpty(inputString))
+      {
+        Console.WriteLine("Enter your string: ");
+        inputString = Console.ReadLine();
+        if (inputString.Equals(string.Empty))
+        {
+          Console.WriteLine("\nYour string is empty. If you want to exit the program, press 'escape' key. If you want to try to enter your string again, press any key besides 'escape'.");
+          if ((Console.ReadKey().Key).Equals(ConsoleKey.Escape))
+          {
+            return;
+          }
+        }
+      }
+      StringProcessing outputString = new StringProcessing();
+      Console.WriteLine(outputString.ProcessTheString(inputString));
+    }
+  }
+}
---
+++ ModifiedString/StringProcessing.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace ModifiedString
+{
+  /// <summary>
+  /// This class was created for work with the input string.
+  /// </summary>
+  class StringProcessing
+  {
+    /// <summary>
+    /// This method processes the input string.
+    /// </summary>
+    /// <param name="inputString"> It is a string entered by user. </param>
+    /// <returns> It returns new, modificated, string. </returns>
+    public string ProcessTheString(string inputString)
+    {
+      StringBuilder modificatedString = new StringBuilder();
+      for (int i = 0; i < inputString.Length; i = i + 2)
+      {
+        modificatedString.Append(inputString[i]);
+      }
+      return modificatedString.ToString();
+    }
+  }
+}
