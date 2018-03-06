13120cabf17b3bd3b46307c4a0b8378de35b0dfd
TatianaGantievskaya <ta.gantievskaya@gmail.com>
2018-03-06 21:30:43 +0300

    add the task

---
+++ NumberConverter/ConvertingException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NumberConverter
+{
+  /// <summary>
+  /// This class was made for possible converting exception.
+  /// </summary>
+  class ConvertingException : Exception
+  {
+    /// <summary>
+    /// Overridden message.
+    /// </summary>
+    public string Message
+    {
+      get
+      {
+        return "You entered wrong radix or incorrect convertible number.";
+      }
+    }
+  }
+}
