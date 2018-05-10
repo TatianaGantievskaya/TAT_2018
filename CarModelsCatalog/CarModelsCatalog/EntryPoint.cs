23640dbbfd591cef00c7cfb68f6aa346f77bb528
TatianaGantievskaya <ta.gantievskaya@gmail.com>
2018-05-10 17:41:18 +0300

    add the task

---
+++ CarModelsCatalog/CarModelsCatalog/EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarModelsCatalog
+{
+  class EntryPoint
+  {
+    static void Main(string[] args)
+    {
+      IWebDriver driver = new ChromeDriver();
+      MainPage mainPage = new MainPage(driver);
+      BrandPage brandPage = mainPage.ValidateTheExistanceAndGoToTheBrandPage(driver, args[0]);
+      List<Model> listOfModels = new List<Model>();
+      foreach(var model in driver.FindElements(By.XPath("//ul[@class='brandslist']//li//a")))
+      {
+        listOfModels.Add(new Model(model.FindElement(By.TagName("span")).Text, int.Parse(model.FindElement(By.TagName("small")).Text)));
+      }
+      InformationOutput.PrintTheListOfModels(listOfModels.OrderByDescending(model => model.CarsAmount).ToList());
+      driver.Quit();
+    }
+  }
+}
---
+++ CarModelsCatalog/CarModelsCatalog/InformationOutput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarModelsCatalog
+{
+  static class InformationOutput
+  {
+    public const string MODEL = "\n\nModel:";
+    public const string AMOUNT = "\nAmount:";
+
+    public static void PrintTheListOfModels(List<Model> listOfModels)
+    {
+      foreach(var model in listOfModels)
+      {
+        Console.WriteLine($"{MODEL} {model.ModelName} {AMOUNT} {model.CarsAmount}");
+      }
+    }
+  }
+}
